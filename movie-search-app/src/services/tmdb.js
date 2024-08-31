19f965f0f37e629ee5b46731ae3ee42d556a4348
Jenna <88835851+itcodegirl@users.noreply.github.com>
2024-08-31 15:20:46 -0500

    Update tmdb.js
    Update your fetchMovies function in src/services/tmdb.js to accept these filters

--- movie-search-app/src/services/tmdb.js
+++ movie-search-app/src/services/tmdb.js
@@ -3,11 +3,12 @@ import axios from 'axios';
 const API_KEY = '2bb0a56b111ff969bea0f1f74ee98e73';
 const BASE_URL = 'https://api.themoviedb.org/3';
 
-const fetchMovies = (query) => {
+const fetchMovies = (query, filters = {}) => {
 	return axios.get(`${BASE_URL}/search/movie`, {
 		params: {
 			api_key: API_KEY,
 			query: query,
+			...filters, // Spread the filters object into the API request
 		},
 	});
 };
