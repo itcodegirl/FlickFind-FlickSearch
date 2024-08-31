2e6f5936eba97f45109319635e2457af40faca46
Jenna <88835851+itcodegirl@users.noreply.github.com>
2024-08-31 15:22:02 -0500

    Update MovieList.js
     MovieList.js, added a button or another UI element that calls onAddToWatchlist when clicked.

--- movie-search-app/src/components/MovieList.js
+++ movie-search-app/src/components/MovieList.js
@@ -1,14 +1,10 @@
 import React from 'react';
 
-const MovieList = ({ movies, onSelectMovie }) => {
+const MovieList = ({ movies, onSelectMovie, onAddToWatchlist }) => {
 	return (
 		<div className="row">
 			{movies.map((movie) => (
-				<div
-					className="col-md-4 mb-3"
-					key={movie.id}
-					onClick={() => onSelectMovie(movie.id)}
-				>
+				<div className="col-md-4 mb-3" key={movie.id}>
 					<div className="card">
 						<img
 							src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
@@ -17,6 +13,10 @@ const MovieList = ({ movies, onSelectMovie }) => {
 						/>
 						<div className="card-body">
 							<h5 className="card-title">{movie.title}</h5>
+							<p className="card-text">{movie.overview}</p>
+							<button className="btn btn-primary" onClick={() => onAddToWatchlist(movie)}>
+								Add to Watchlist
+							</button>
 						</div>
 					</div>
 				</div>
