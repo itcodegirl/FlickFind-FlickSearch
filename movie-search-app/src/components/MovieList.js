9e5ea5f711ce079537f6fdae2edad8e8d07644a3
Jenna <88835851+itcodegirl@users.noreply.github.com>
2024-09-03 12:04:40 -0500

    Update MovieList.js

--- movie-search-app/src/components/MovieList.js
+++ movie-search-app/src/components/MovieList.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
-const MovieList = ({ movies = [] }) => {
+const MovieList = ({ movies = [], onAddToWatchlist }) => {
 	return (
 		<div>
 			{movies.map((movie, index) => (
 				<div key={index}>
 					<h3>{movie.title}</h3>
 					<p>{movie.description}</p>
+					<button onClick={() => onAddToWatchlist(movie)}>Add to Watchlist</button>
 				</div>
 			))}
 		</div>
