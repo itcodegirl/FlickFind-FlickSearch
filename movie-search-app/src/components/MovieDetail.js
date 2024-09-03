b856a9fb2dd5adca1231a02a978ff2bbeaa88827
Jenna <88835851+itcodegirl@users.noreply.github.com>
2024-09-03 16:57:25 -0500

    Update MovieDetail.js

--- movie-search-app/src/components/MovieDetail.js
+++ movie-search-app/src/components/MovieDetail.js
@@ -2,6 +2,10 @@
 import React, { useEffect, useState } from 'react';
 import { fetchMovieDetails } from '../services/tmdb';
 import { ClipLoader } from 'react-spinners';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
 
 const MovieDetail = ({ movieId }) => {
 	const [movie, setMovie] = useState(null);
@@ -27,11 +31,24 @@ const MovieDetail = ({ movieId }) => {
 	);
 
 	return (
-		<div>
-			<h2>{movie.title}</h2>
-			<p>{movie.overview}</p>
-			<img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
-		</div>
+		<Box sx={{ mt: 4 }}>
+			<Grid container spacing={4}>
+				<Grid item xs={12} md={4}>
+					<Paper elevation={3}>
+						<img
+							src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+							alt={movie.title}
+							style={{ width: '100%' }}
+						/>
+					</Paper>
+				</Grid>
+				<Grid item xs={12} md={8}>
+					<Typography variant="h4" gutterBottom>{movie.title}</Typography>
+					<Typography variant="body1" gutterBottom>{movie.overview}</Typography>
+					{/* Additional details like cast, crew, and related movies can be added here */}
+				</Grid>
+			</Grid>
+		</Box>
 	);
 };
 
